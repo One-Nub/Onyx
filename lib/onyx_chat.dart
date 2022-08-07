af99fd478b25d3b7e08131eaf84af9af585c64a8
Nub <38899321+One-Nub@users.noreply.github.com>
2022-08-07 01:01:00 -0500

    Remove Onyx chat functionality
    Onyx chat functionality has been moved to it's own repository here: https://github.com/One-Nub/Onyx_Chat

--- lib/onyx_chat.dart
+++
@@ -1,9 +0,0 @@
-/// Utilize text/chat based command features with Onyx.
-library onyx_chat;
-
-export 'src/chat/structures/text_command.dart';
-export 'src/chat/structures/text_context.dart';
-export 'src/chat/structures/text_subcommand.dart';
-
-export 'src/converters.dart';
-export 'src/chat/onyx_chat.dart';
