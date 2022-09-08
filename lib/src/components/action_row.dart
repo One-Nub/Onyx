36a968574b1864356dc8dadd2ba34cadb1419d95
Nub <38899321+One-Nub@users.noreply.github.com>
2022-09-07 22:14:47 -0500

    Add .fromJson() constructor

--- lib/src/components/action_row.dart
+++ lib/src/components/action_row.dart
@@ -1,6 +1,9 @@
 import '../enums.dart';
 import '../typedefs.dart';
+import 'button.dart';
 import 'component_base.dart';
+import 'select_menu.dart';
+import 'text_input.dart';
 
 class ActionRow implements Component {
   final ComponentType type = ComponentType.action_row;
@@ -8,6 +11,21 @@ class ActionRow implements Component {
 
   ActionRow({ComponentList? components}) {
     this.components = [...?components];
+  }
+
+  ActionRow.fromJson(JsonData data) {
+    components = [];
+    (data["components"] as List).forEach((element) {
+      ComponentType elementType = ComponentType.fromInt(element["type"]);
+
+      if(elementType == ComponentType.button) {
+        components.add(Button.fromJson(element));
+      } else if (elementType == ComponentType.select_menu) {
+        components.add(SelectMenu.fromJson(element));
+      } else if (elementType == ComponentType.text_input) {
+        components.add(TextInput.fromJson(element));
+      }
+    });
   }
 
   void addComponent(Component component) => components.add(component);
